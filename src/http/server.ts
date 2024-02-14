050fe4a036b5f4954ee7b94765153392cde059cf
Miott0 <miotterafael@gmail.com>
2024-02-14 14:30:23 -0300

    feat: finish sprint

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,8 @@
 import fastify from 'fastify'
+import websocket from "@fastify/websocket";
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
-
+import { pollResults } from "./ws/poll-results";
 import cookie from "@fastify/cookie"
 import { voteOnPoll } from './routes/vote-on-poll'
 
@@ -16,11 +17,14 @@ app.register(cookie,{
     
 })
 
+app.register(websocket)
+
     //POLL
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
 
+app.register(pollResults)
 
 //SERVER
 app.listen({port: 3333}).then(() =>{
